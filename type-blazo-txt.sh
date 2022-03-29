ba4d960bdd690207c96b2860ba08e30860b9fa97
Marcin-Blazowski <88790554+Marcin-Blazowski@users.noreply.github.com>
2022-03-29 09:30:19 +0200

    Update type-blazo-txt.sh

--- type-blazo-txt.sh
+++ type-blazo-txt.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Variables
-fileServicePrimaryEndpoint=$1
-
-# Parameters validation
-if [[ -z $fileServicePrimaryEndpoint ]]; then
-    echo "fileServicePrimaryEndpoint parameter cannot be null or empty"
-    exit 1
-fi
-
-echo $fileServicePrimaryEndpoint > $HOME/test-script-log.txt
+echo "id = " > $HOME/test-script-log.txt
+id >> $HOME/test-script-log.txt
+echo "whoami" >> $HOME/test-script-log.txt
+echo "pwd" >> $HOME/test-script-log.txt
+pwd >> $HOME/test-script-log.txt
+echo "uname -a" >> $HOME/test-script-log.txt
+uname -a >> $HOME/test-script-log.txt
+echo "hostname" >> $HOME/test-script-log.txt
+hostname >> $HOME/test-script-log.txt
+echo " "
 echo "Tony Halik: Tu bylem!" >> $HOME/test-script-log.txt
