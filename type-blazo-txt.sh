91da81d9b57428d10b4c8a23338560d479d1b009
Marcin-Blazowski <88790554+Marcin-Blazowski@users.noreply.github.com>
2022-03-29 13:38:12 +0200

    Update type-blazo-txt.sh

--- type-blazo-txt.sh
+++ type-blazo-txt.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-echo "HOME ="
+echo "HOME =" >> $HOME/test-script-log.txt
 echo $HOME >> $HOME/test-script-log.txt
 HOME=~
+echo "new HOME =" >> $HOME/test-script-log.txt
 echo $HOME >> $HOME/test-script-log.txt
 cd ~
 echo "id = " >> $HOME/test-script-log.txt
