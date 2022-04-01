a10a5f7f1fef29adafb0455d110905564dd51914
Marcin Blazowski <marcin.blazowski@kyndryl.com>
2022-04-01 10:37:10 +0200

    minor updates

--- azure/ubuntu/azure/mount-azure-fileshare.sh
+++ azure/ubuntu/azure/mount-azure-fileshare.sh
@@ -2,9 +2,7 @@
 # $1 = Azure storage account name
 # $2 = Azure storage account key
 # $3 = Azure file share name
-# $4 = mountpoint path
-# $5 - username
-# $6 - storageAccount suffix
+# $4 = storageAccount suffix
 
 # For more details refer to https://azure.microsoft.com/en-us/documentation/articles/storage-how-to-use-files-linux/
 
@@ -13,13 +11,13 @@ apt-get -y update
 
 # install cifs-utils and mount file share
 apt-get install cifs-utils
-mkdir -p $4
-echo "mount -t cifs //$1.file.$6/$3 $4 -o vers=3.0,username=$1,password=$2,dir_mode=0755,file_mode=0664" >> /tmp/azure-mount-log.txt
-mount -t cifs //$1.file.$6/$3 $4 -o vers=3.0,username=$1,password=$2,dir_mode=0755,file_mode=0664 >> /tmp/azure-mount-log.txt
 
-# create a symlink from /mountpath/xxx to ~username/xxx
-linkpoint=`echo $4 | sed 's/.*\///'`
-eval ln -s $4 ~$5/$linkpoint
+# set mount point path and create folder if missing
+mountPointPath = /mnt/k8s-share
+mkdir -p $mountPointPath
+
+echo "mount -t cifs //$1.file.$4/$3 $mountPointPath -o vers=3.0,username=$1,password=$2,dir_mode=0755,file_mode=0664" >> /tmp/azure-mount-log.txt
+mount -t cifs //$1.file.$4/$3 $mountPointPath -o vers=3.0,username=$1,password=$2,dir_mode=0755,file_mode=0664 >> /tmp/azure-mount-log.txt
 
 # create marker files for testing
-echo "hello from $HOSTNAME" > $4/$HOSTNAME.txt
+echo "hello from $HOSTNAME" > $mountPointPath/$HOSTNAME.txt
---
+++ azure/ubuntu/azure/put-hostname-prefixes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+MasterPrefix=$1
+WorkerPrefix=$2
+# mkdir -p /mnt/k8s-share
+cd /mnt/k8s-share
+echo $HOSTNAME $uri >> /mnt/k8s-share/k8s-hostname-prefixes.txt
+# if this is master-1 VM
+if [ "$HOSTNAME" == "master-1" ]
+then
+  echo master-prefix $MasterPrefix >> /mnt/k8s-share/k8s-hostname-prefixes.txt
+  echo worker-prefix $WorkerPrefix >> /mnt/k8s-share/k8s-hostname-prefixes.txt
+fi
