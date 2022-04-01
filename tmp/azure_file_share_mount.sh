c0306b312b2c22762d0384f551d20ca715c05e25
Marcin Blazowski <marcin.blazowski@kyndryl.com>
2022-04-01 16:21:53 +0200

    fix sa key expose

--- tmp/azure_file_share_mount.sh
+++ tmp/azure_file_share_mount.sh
@@ -4,7 +4,7 @@ sudo mkdir /etc/smbcredentials
 fi
 if [ ! -f "/etc/smbcredentials/k8steststorage.cred" ]; then
     sudo bash -c 'echo "username=k8steststorage" >> /etc/smbcredentials/k8steststorage.cred'
-    sudo bash -c 'echo "password=eg0J13A4Ifoa4e8p+S7CsiiZlwQsElOzKZor7Tpksh/j729D0N4fDMNGhAzM8XpjCGID8H3r3LQpNrludLb/dg==" >> /etc/smbcredentials/k8steststorage.cred'
+    sudo bash -c 'echo "password=storagekey" >> /etc/smbcredentials/k8steststorage.cred'
 fi
 sudo chmod 600 /etc/smbcredentials/k8steststorage.cred
 
