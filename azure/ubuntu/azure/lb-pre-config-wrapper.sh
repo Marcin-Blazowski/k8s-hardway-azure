45b4f43efb53e6613154d579df2dfcbe8825c6c4
Marcin Blazowski <marcin.blazowski@kyndryl.com>
2022-04-04 16:41:21 +0200

    azure wrapper config script initial

---
+++ azure/ubuntu/azure/lb-pre-config-wrapper.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# this is wrapper to execute scripts for loadbalancer node pre-configuration step
+# update hosts file
+/mnt/k8s-share/repo/k8s-hardway-azure/azure/ubuntu/azure/setup-hosts.sh
