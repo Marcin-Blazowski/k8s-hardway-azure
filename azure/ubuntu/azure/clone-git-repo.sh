b2fcc17fe2c2a712dd391daec960894590e3d05f
Marcin Blazowski <marcin.blazowski@kyndryl.com>
2022-04-04 18:15:57 +0200

    wrapper scripts update

--- azure/ubuntu/azure/clone-git-repo.sh
+++ azure/ubuntu/azure/clone-git-repo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Tony Halik tu bylem i git clone odpalilem"
 uri=$1
-# mkdir -p /mnt/k8s-share/repo
+mkdir -p /mnt/k8s-share/repo
 cd /tmp
 echo $HOSTNAME $uri >> /mnt/k8s-share/repo/uri.txt
 git clone $uri
