96e97992ca1b26e77bc87f282bb04991dadd4dc0
Marcin Blazowski <marcin.blazowski@kyndryl.com>
2022-04-04 18:00:44 +0200

    wrapper scripts update

--- azure/ubuntu/azure/clone-git-repo.sh
+++ azure/ubuntu/azure/clone-git-repo.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 echo "Tony Halik tu bylem i git clone odpalilem"
 uri=$1
-mkdir -p /mnt/k8s-share/repo
-cd /mnt/k8s-share/repo
+# mkdir -p /mnt/k8s-share/repo
+cd /tmp
 echo $HOSTNAME $uri >> /mnt/k8s-share/repo/uri.txt
-# if this is master-1 VM
-if [ "$HOSTNAME" == "master-1" ]
-then
-  git clone $uri
-fi
+git clone $uri
