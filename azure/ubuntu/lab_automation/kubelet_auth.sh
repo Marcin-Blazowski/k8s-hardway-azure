82ce168393f168a6da7ab4f91607b6792c6af3cd
Marcin Blazowski <marcin.blazowski@kyndryl.com>
2022-04-05 22:12:17 +0200

    minor kubelet fix

--- azure/ubuntu/lab_automation/kubelet_auth.sh
+++ azure/ubuntu/lab_automation/kubelet_auth.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 HOSTNAME=$(hostname -s)
 
-cp /mnt/k8s-share/azure/ubuntu/lab_automation/cluster_role_nodes.yaml $HOME/
-cp /mnt/k8s-share/azure/ubuntu/lab_automation/cluster_role_binding_nodes.yaml $HOME/
+cp /tmp/k8s-hardway-azure/azure/ubuntu/lab_automation/cluster_role_nodes.yaml $HOME/
+cp /tmp/k8s-hardway-azure/ubuntu/lab_automation/cluster_role_binding_nodes.yaml $HOME/
 
 kubectl apply -f $HOME/cluster_role_nodes.yaml
 kubectl apply -f $HOME/cluster_role_binding_nodes.yaml
